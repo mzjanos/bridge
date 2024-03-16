a0997ad40bff3269251cc38a735bf9916a596a3b
markjano <mzjanos2002@gmail.com>
2024-03-16 23:30:22 +0100

    project started not much done

---
+++ Suit.h
@@ -0,0 +1,11 @@
+#ifndef SUIT_H
+#define SUIT_H
+
+enum class Suit{
+    Spade,
+    Hearth,
+    Diamond,
+    Club
+}
+
+#endif
---
+++ card.h
@@ -0,0 +1,11 @@
+#ifndef CARD_H
+#define CARD_H
+
+class Card{
+    private:
+    int value;
+    Suit color; 
+     
+}
+
+#endif 
